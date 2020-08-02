d7e2d66ac3b34872288591c26afa2d4343608271
guiamado <amado.guigui@gmail.com>
2020-08-02 17:22:55 -0300

    [ADD] Adicionando modal bottom sheet com long pressed do carro

--- lib/pages/carro/carros_listview.dart
+++ lib/pages/carro/carros_listview.dart
@@ -17,47 +17,55 @@ class CarrosListView extends StatelessWidget {
         itemCount: carros != null ? carros.length : 0,
         itemBuilder: (context, index) {
           Carro c = carros[index];
-          return Card(
-            color: Colors.grey[100],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: CachedNetworkImage(
-                      imageUrl: c.urlFoto ??
-                          'https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Ferrari_FF.png',
-                      width: 250,
-                    ),
-                  ),
-                  Text(
-                    c.nome,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text(
-                    'Descrição...',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  ButtonBar(
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text('DETALHES'),
-                        onPressed: () => _onClickCarro(context, c),
+          return InkWell(
+            onTap: () {
+              _onClickCarro(context, c);
+            },
+            onLongPress: () {
+              _onLongClickCarro(context, c);
+            },
+            child: Card(
+              color: Colors.grey[100],
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: CachedNetworkImage(
+                        imageUrl: c.urlFoto ??
+                            'https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Ferrari_FF.png',
+                        width: 250,
                       ),
-                      FlatButton(
-                        child: const Text('SHARE'),
-                        onPressed: () {/* ... */},
+                    ),
+                    Text(
+                      c.nome,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 25,
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    Text(
+                      'Descrição...',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text('DETALHES'),
+                          onPressed: () => _onClickCarro(context, c),
+                        ),
+                        FlatButton(
+                          child: const Text('SHARE'),
+                          onPressed: () => _onClickShare(context, c),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           );
@@ -68,5 +76,50 @@ class CarrosListView extends StatelessWidget {
 
   _onClickCarro(context, Carro c) {
     push(context, CarroPage(c));
+  }
+
+  void _onLongClickCarro(BuildContext context, Carro c) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  c.nome,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+              ListTile(
+                title: Text('Detalhes'),
+                leading: Icon(
+                  Icons.directions_car,
+                  color: Colors.blue,
+                ),
+                onTap: () {
+                  pop(context);
+                  _onClickCarro(context, c);
+                },
+              ),
+              ListTile(
+                title: Text('Share'),
+                leading: Icon(
+                  Icons.share,
+                  color: Colors.blue,
+                ),
+                onTap: () {
+                  pop(context);
+                  _onClickShare(context, c);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  void _onClickShare(BuildContext context, Carro c) {
+    print('share ${c.nome}');
   }
 }
